fe44605f0d7dd7142fc16d74897baa9a2bf342c4
Ron Kokotov <2ron.kokotov@gmail.com>
2023-01-10 01:08:27 +0200

    added an important line to client simulation

--- RealTimeProject/Graphics.cs
+++ RealTimeProject/Graphics.cs
@@ -277,6 +277,7 @@ namespace RealTimeProject
                             }
                         }
                     }
+                    break;
                 }
             }
             Draw(simHistory.Last().state);
